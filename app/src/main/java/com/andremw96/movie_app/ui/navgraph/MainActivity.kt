2ef91c2da1f5315bb59e51a0e6e12e825f7e5d1d
andrea <andrea.wijaya@advancegroup.com>
2023-11-03 15:41:24 +0700

    add splas screen

--- app/src/main/java/com/andremw96/movie_app/ui/navgraph/MainActivity.kt
+++ app/src/main/java/com/andremw96/movie_app/ui/navgraph/MainActivity.kt
@@ -3,6 +3,7 @@ package com.andremw96.movie_app.ui.navgraph
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.andremw96.movie_app.ui.theme.MovieAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppTheme {
